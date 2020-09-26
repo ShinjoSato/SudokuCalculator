e37b9bfd9c77aa24d88836dfc04bbff4f6b5afaf
Shinjo Sato <sxs1640@student.bham.ac.uk>
2020-09-27 00:47:38 +0900

    start creating an GUI; update server.py.

--- server.py
+++ server.py
@@ -6,23 +6,24 @@ from src.main import main
 class SudokuHandler(server.BaseHTTPRequestHandler):
     def do_POST(self):
         # リクエスト取得
+        print("Get a request.")
         content_len  = int(self.headers.get("content-length"))
         body = json.loads(self.rfile.read(content_len))
         result=main(body)
         response={
-            'comment': 'It works!',
             'result': result
         }
         #Response
         self.send_response(200)
         self.send_header('Content-type', 'application/json;')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         body_json = json.dumps(response, sort_keys=False, indent=4, ensure_ascii=False) 
         self.wfile.write(body_json.encode("utf-8"))
 
 
 # サーバ起動
-host = '0.0.0.0'
+host = '192.168.0.5'
 port = 3333
 httpd = server.HTTPServer((host, port), SudokuHandler)
 httpd.serve_forever()
